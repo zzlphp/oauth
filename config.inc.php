ca393e83e96af220105f893c1acc0983d51ab3b6
张子良 <zhangziliang@fangjinsuo.com>
2017-02-16 13:53:46 +0800

    oauth

---
+++ config.inc.php
@@ -0,0 +1,9 @@
+<?php
+// 数据库连接信息
+$dbOptions = array(
+    'server'   => 'localhost',
+    'username' => 'root',
+    'password' => 'liang123',
+    'database' => 'oauth'
+);
+?>
---
+++ request_token.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ziziliang
+ * Date: 2017/2/16
+ * Time: 下午1:51
+ */
+include_once 'config.inc.php';
+include_once 'oauth-php/library/OAuthStore.php';
+include_once 'oauth-php/library/OAuthServer.php';
+
+$store = OAuthStore::instance('MySQL', $dbOptions);
+
+$server = new OAuthServer();
+$server->requestToken();
+exit();
+?>
