492b45b0882e56c2c1a5171130cb9018a48eaaa8
张子良 <zhangziliang@fangjinsuo.com>
2017-02-17 16:15:01 +0800

    oauth

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -9,6 +9,10 @@ class IndexController extends Controller {
 
     public function index()
     {
+        $client_id   = 1000;
+        $type        = 'zhangziliang';
+        $url = "http://api.zzlphp.com/home/oauth/authorize?redirect_uri=http://o.zzlphp.com/home/login/index/client_id/{$client_id}/type/{$type}&response_type=code&client_id={$client_id}&type={$type}";
+        $this->assign('url',$url);
         $this->display();
     }
 }
